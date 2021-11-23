f55fe633c6a3a24da286e1a76d99f699d3bdf03b
michael novella <michaelb.novella@gmail.com>
2021-11-23 11:56:15 -0800

    added some empty files for the process of database management might not be enough,but will be there to send and query from the sql server

---
+++ send.php
@@ -0,0 +1,8 @@
+<?php
+//this is to send the info from the ended game to the server
+$servername = "localhost"; // default server name
+$username = "hubert"; // user name that you created
+$password = "OXccs8wmVCGDU0fG"; // password that you created
+//$dbname = "myDB"; need to set this to what will be the database that will contain all of the information about the game
+
+?>
