02828460426100313759976f263fca99eb3cf1fa
michael novella <michaelb.novella@gmail.com>
2021-12-11 14:08:25 -0800

    made this the header to go ontop of the pages

---
+++ heading-nav.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>
+    </head>
+    <body>
+        <header>
+            <nav>
+                <a>
+                    <img src="img/fresno-state-bulldogs.svg" alt="fresnostate logo">
+                </a>
+                <ul>
+                    <li><a href="main.php">Main</a></li>
+                    <li><a href="help.php">Help</a></li>
+                    <li><a href="contact.html">Contact</a></li>
+                </ul>
+                <div>
+                    <form action="login.php" method="post">
+                        <input type="text" name="userid" placeholder="Username or email">
+                        <input type="text" name="passd" placeholder="Password">
+                        <button type="submit" name="login">press to Login</button>
+                    </form>
+                    <a href="signup.php">Signup</a>
+                    <form action=""
+                </div>
+            </nav>
+        </header>
+    </body>
+</html>
