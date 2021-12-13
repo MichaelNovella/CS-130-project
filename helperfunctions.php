f9fe0ba73c1ec6adb90ec6aed586c6487beab005
michael novella <michaelb.novella@gmail.com>
2021-12-12 16:41:31 -0800

    added default connection

--- helperfunctions.php
+++ helperfunctions.php
@@ -3,6 +3,9 @@ $servername = "localhost"; // default server name
 $username = "AdminLab12"; // user name that you created
 $password = "4VPnroTOC6wOU3mn"; // password that you created
 $dbname = "myDB"; //set this to be the table it will read from and what info is needed to be returned
-
-
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+if (!$conn) {
+    die("Connection failed: ". $conn->connect_error);
+}
 ?>
