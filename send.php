130b4d6eec41df7b91bac02d6f4b46f78a759745
michael novella <michaelb.novella@gmail.com>
2021-12-12 18:34:13 -0800

    added functionality to send to database

--- send.php
+++ send.php
@@ -1,5 +1,21 @@
 <?php
 //this is to send the info from the ended game to the server
 include "helperfunctions.php";
+// turns='+ turn+'&score='+ score+'&duration='+ duration+'&winner'+ winner
+// $sql = "CREATE TABLE IF NOT EXISTS userboard (
+//     id INT(6) PRIMARY KEY,
+//     turns INT(30),
+//     score INT(30),
+//     gameswon INT(6),
+//     games INT(6),
+//     duration VARCHAR(190) 
+//     )";
+// if ($conn->query($sql) === TRUE) {
+//     //echo "New record created successfully<br>";
+// } else {
+//     //echo "Error: " . $sql . "<br>" . $connection->error ."<br>";
+// }
 
+$sql="";
+$stmt = $connection->prepare("INSERT INTO userboard ");
 ?>
--- signup.inc.php
+++ signup.inc.php
@@ -73,6 +73,7 @@ if(isset($_POST["signupsub"])){
         }
         
     }
+    
     $stmt->close();
     $conn->close();
 
