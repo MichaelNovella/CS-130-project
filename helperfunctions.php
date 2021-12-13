eec204f00763a239c0fb96b7683b0b0f8465c023
michael novella <michaelb.novella@gmail.com>
2021-12-12 18:34:46 -0800

    creates table

--- helperfunctions.php
+++ helperfunctions.php
@@ -8,4 +8,17 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if (!$conn) {
     die("Connection failed: ". $conn->connect_error);
 }
+$sql = "CREATE TABLE IF NOT EXISTS userboard (
+    ids VARCHAR(30),
+    turns INT(30),
+    score INT(30),
+    gameswon INT(6),
+    games INT(6),
+    duration INT(190) 
+    )";
+if ($conn->query($sql) === TRUE) {
+    //echo "New record created successfully<br>";
+} else {
+    //echo "Error: " . $sql . "<br>" . $connection->error ."<br>";
+}
 ?>
