68bff15a1494dad6444e35b32fbed6a8e7f319ba
michael novella <michaelb.novella@gmail.com>
2021-12-12 16:41:52 -0800

    checks to see if the user is signed in

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+require "heading-nav.php";
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+    <link rel="stylesheet" href="external.css">
+    </head>
+    <body>
+        <div class="container">
+        <?php
+        if (isset($_SESSION['userid'])) {
+            include 'Gomoku_WIP.php';
+        }
+        else{
+            echo '<p>You have to log in</p>';
+        }
+        ?>
+        </div>
+    </body>
+</html> 
