deac92b720fd2f04c1f52dcc848a932562fc3c9b
michael novella <michaelb.novella@gmail.com>
2021-12-12 16:42:59 -0800

    signup functions

---
+++ signup.inc.php
@@ -0,0 +1,83 @@
+<?php
+
+// <input type="text" name="username" placeholder="Username">
+// </div>
+// </div>
+// <div class="row">
+// <div class="col-75">
+//     <input type="text" name="useremail" placeholder="Email">
+// </div>
+// </div>
+// <div class="row">
+// <div class="col-75">
+//     <input type="password" name="passwrd" placeholder="Password">
+// </div>
+// </div>
+// <div class="row">
+// <div class="col-75">
+//     <input type="password" name="verify-passwrd" placeholder="Verify Password">
+// </div>
+// </div>
+// <div class="row">
+// <div class="col-75">
+//     <button type="submit" name="signupsub">Signup</button>
+// </div>
+// </div>
+if(isset($_POST["signupsub"])){
+    require 'helperfunctions.php';
+
+    $username = $_POST["username"];
+    $email = $_POST["useremail"];
+    $pwd = $_POST["passwrd"];
+    $pwdver = $_POST["verify-passwrd"];
+
+    if (empty($username) || empty($email)|| empty($pwd)|| empty($pwdver)) {
+        header("Location: signup.php?error=emptyfield&username=".$username."&useremail=".$email);
+        exit();
+    }
+    elseif (!filter_var($email, FILTER_VALIDATE_EMAIL) && !preg_match("/^[a-zA-Z0-9]*$/",$username)) {
+        header("Location: signup.php?error=invalididendity");
+        exit();
+    }
+    elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        header("Location: signup.php?error=invalidmain&username=".$username);
+        exit();
+    }
+    elseif (!preg_match("/^[a-zA-Z0-9]*$/",$username)) {
+        header("Location: signup.php?error=invalidid&email=".$email);
+        exit();
+    }
+    elseif ($pwd !== $pwdver) {
+        header("Location: signup.php?error=passwordnotmatch&username=".$username."&useremail=".$email);
+        exit();
+    }
+    else {
+        $sql = "SELECT usernamesid FROM users WHERE usernamesid=?";
+        $stmt = $conn->prepare($sql); 
+        $stmt->bind_param("s", $username);
+        $stmt->execute();
+        $stmt->store_result();
+        $results = $stmt->num_rows;
+        if ($results >0 ) {
+            header("Location: signup.php?error=usertaken&useremail=".$email);
+            exit();
+        }
+        else {
+            $sql = "INSERT INTO  users (usernamesid,email_id,pwdusers) VALUES(?,?,?)";
+            $hasspasse = password_hash($pwd, PASSWORD_DEFAULT);
+            $stmt = $conn->prepare($sql); 
+            $stmt->bind_param("sss", $username,$email,$hasspasse);
+            $stmt->execute();
+            header("Location: signup.php?signup=passed");
+            exit();
+        }
+        
+    }
+    $stmt->close();
+    $conn->close();
+
+}
+else {
+    header("Location: signup.php");
+    exit();
+}
--- signup.php
+++ signup.php
@@ -1,2 +1,44 @@
 <?php
+require "heading-nav.php";
 ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <!-- <link rel="stylesheet" href="external.css"> -->
+        <style>
+            
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <form action="signup.inc.php" method="post">
+            <div class="row">
+                <h1>Sign Up</h1>
+                <div class="col-75">
+                    <input type="text" name="username" placeholder="Username">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-75">
+                    <input type="text" name="useremail" placeholder="Email">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-75">
+                    <input type="password" name="passwrd" placeholder="Password">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-75">
+                    <input type="password" name="verify-passwrd" placeholder="Verify Password">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-75">
+                    <button type="submit" name="signupsub">Signup</button>
+                </div>
+            </div>
+        </form>
+    </div>
+</body>
+</html> 
